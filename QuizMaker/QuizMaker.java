ccb9174903484dc3b20a28589b7285abfa3e2185
agutteridge <alicegutteridge@gmail.com>
2014-04-02 17:44:50 +0100

    Adding to create mode, checking quiz name and IDs OK.

--- QuizMaker/QuizMaker.java
+++ QuizMaker/QuizMaker.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
+import java.rmi.Remote;
+import java.io.*;
+import java.rmi.Naming;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.rmi.NotBoundException;
 
 public class QuizMaker {
-	
 
 	public void launch(){
 		Scanner in = new Scanner(System.in);
@@ -26,24 +31,36 @@ public class QuizMaker {
 
 
 	private void create(){
-		System.out.println("*EDIT MODE*");
+		System.out.println("*CREATE MODE*");
 
 		Scanner in = new Scanner(System.in);
-		boolean finalName = false;
+		boolean isFinal = false;
+		String quizName = "";
+		String first4chars = "";
 
 		do {
-			System.out.println("Please enter a name for your quiz:");
+			System.out.println("Please enter a name for your quiz (4 chars min, 30 chars max.):");
 			String str = in.nextLine();
-			System.out.println("Is the name " + str + " acceptable?");
-			String ans = in.nextLine();
-			finalName = yesNo(ans);
-		} while (finalName = false);
 
-		String quizName = str.substring(0,4);
+			if (str.length() < 4){
+				isFinal = false;
+				System.out.println("Sorry, the quiz name must be at least 4 characters long.");
+			} else {
+				isFinal = yesNo(str);
+				first4chars = str.substring(0,4);
+				quizName = str;
+			}
+		} while (!isFinal);
+
+		String quizID = "";
 		try {
 			Remote service = Naming.lookup("//127.0.0.1:1099/quiz");
 			Compute compute = (Compute) service;
-			compute.checkQuizName();
+			quizID = compute.generateUniqueQuizID(first4chars);
+			if (quizID == null){
+				throw new NullPointerException();
+			}
+			System.out.println("The ID of your quiz \"" + quizName + "\" is " + quizID);
 		} catch (MalformedURLException ex) {
 			ex.printStackTrace();
 		} catch (RemoteException ex) {
@@ -51,20 +68,25 @@ public class QuizMaker {
 		} catch (NotBoundException ex) {
 			ex.printStackTrace();
 		}
- 
 	}
 
 	private boolean yesNo(String str){
 		isQuit(str);
-		str = str.toUpperCase();
-		if (str.equals("Y")){
-			return true;
-		} else if (str.equals("N")){
-			return false
-		} else {
-			System.out.println("Sorry, your entry wasn't recognised.");
-			return false;	
-		}
+		boolean correctInput = false;
+		str = str.substring(0, 31);
+
+		do {
+			System.out.println("Is the name " + str + " acceptable?");
+			String ans = in.nextLine();
+			ans = str.toUpperCase();
+			if (ans.equals("Y")){
+				return true;
+			} else if (ans.equals("N")){
+				return false;
+			} else {
+				System.out.println("Sorry, your entry wasn't recognised.");
+			}
+		} while (!correctInput);
 	}
 
 	private void isQuit(String str){
@@ -76,7 +98,7 @@ public class QuizMaker {
 
 	private void flush(){
 		//making sure all data gets saved 
-		System.exit();
+		System.exit(0);
 	}
 
 	public static void main(String[] args) {
