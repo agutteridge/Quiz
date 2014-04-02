37a7ad74a6b26d56fb0fd4968108f4698453f96c
agutteridge <alicegutteridge@gmail.com>
2014-04-02 13:22:18 +0100

    added policies, making enterName() method

---
+++ QuizPlayer/Compute.java
@@ -0,0 +1,27 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.UUID;
+
+public interface Compute extends Remote {
+
+	/**
+	* Prints a list of quizzes that are available to play.
+	* 
+	* @return void, quizzes are printed (or make CL menu?)
+	*/
+	void listQuizzes() throws RemoteException;
+
+	void enterName(String name) throws RemoteException;
+
+	// void startQuiz(UUID quizID);
+
+	// void sendAnswer(String answer); //enum?
+
+	// void sendPoints(int points);
+
+	// void listHighScores(String nickname); //list in batches of 10, enable scrolldown?
+
+	// void listHighScores(String nickname, UUID quizID); //as above
+
+	// void listHighScores(UUID quizID); //as above
+}
--- QuizPlayer/Main.java
+++ QuizPlayer/Main.java
@@ -8,7 +8,43 @@ import java.rmi.NotBoundException;
 public class Main implements Serializable {
 	public void launch(){
 		System.out.println("Let's list all of the quizzes!");
+		register();
 		listQuizzes();
+	}
+
+	private void enterName(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a username:");
+        boolean alphanum = false;
+        String str;
+	        
+        while (!alphanum) {
+            str = in.nextLine();
+            char[] charArray = str.toCharArray();
+        
+            alphanum = true;
+            for (char c : charArray) {
+                if (!Character.isLetterOrDigit(c)){
+                    alphanum = false;
+                }
+            }
+
+            if(!alphanum){
+                System.out.println("Sorry, only alphanumeric characters can be used. Please enter a username:");
+            }
+        }
+
+		try {
+			Remote service = Naming.lookup("//127.0.0.1:1099/quiz");
+			Compute compute = (Compute) service;
+			compute.enterName(str);
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (RemoteException ex) {
+			ex.printStackTrace();
+		} catch (NotBoundException ex) {
+			ex.printStackTrace();
+		}		
 	}
 
 	private void listQuizzes(){
