18205d5a10538bdc037fab7bac9620499462b724
agutteridge <alicegutteridge@gmail.com>
2014-04-02 18:20:23 +0100

    Added List of questions as member variable

--- Server/Quiz.java
+++ Server/Quiz.java
@@ -2,11 +2,20 @@ public class Quiz {
 	private final String quizName;
 	private final String quizID;
 	private SortedScoreList scoreList;
+	private List<Question> questions;
 
 	public Quiz(String name, String id){
 		quizName = name;
 		quizID = id;
 	    scoreList = new SortedScoreList();
+	}
+
+	public String getName(){
+		return quizName;
+	}
+
+	public String getQuizID(){
+		return quizID;
 	}
 }
 
