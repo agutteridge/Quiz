0e384939eda561b5e345a739be475c39bb16c6d6
agutteridge <alicegutteridge@gmail.com>
2014-04-05 14:05:09 +0100

    Added methods for setting options within Question class of Quiz

--- Server/Quiz.java
+++ Server/Quiz.java
@@ -5,6 +5,7 @@ public class Quiz extends Serializable {
 	private final String quizID;
 	private SortedScoreList scoreList;
 	private List<Question> questions;
+	private boolean pass = false; //evaluates whether quiz is //finished (to be written) or not.
 
 	public Quiz(String name, String id){
 		quizName = name;
@@ -28,7 +29,7 @@ public class Quiz extends Serializable {
 		}
 
 		Question newQuestion = new Question(q);
-
+		
 
 	}
 }
@@ -36,19 +37,39 @@ public class Quiz extends Serializable {
 //inner class if not required to be serializable?
 class Question extends Serializable {
 	private String question;
-	private String[] options;
-	private int correct answer;
-	private boolean pass = false;
+	private List<String> options;
+	private int correct;
+	private boolean pass = false; //evaluates whether question is //finished (to be written) or not.
 
 	public Question(String q){
 		question = q;
+		options = new ArrayList<String>(2);
 	}
 
 	public boolean addOption(String option){
-		int 
-		option[]
+		if (options.size() > 4){
+			return false;
+		} else {
+			options.add(option);
+			return true;
+		}
 	}
 
-	public void listOptions
+	public boolean setCorrect(int answer){
+		if (answer < options.size()){
+			correct = answer;
+			if (options.size() > 1){
+				pass = true;
+			}
+		} 
+	}
+
+	public boolean hasPassed(){
+		return pass;
+	}
+
+	public String getOption(int num){
+		return options.get(num);
+	}
 
 }
